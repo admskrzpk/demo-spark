b0891c824d1d08e2f0ed86453afc96f415a9ebf7
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-05 11:55:20 +0200

    initial commmit

---
+++ src/main/scala/SparkDemo.scala
@@ -0,0 +1,15 @@
+object SparkDemo extends App {
+
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    //.config("spark.some.config.option", "some-value")
+    .master("local[*]")
+    .getOrCreate()
+
+  val df = spark.read.text("build.sbt")
+  df.show(truncate = false)
+  val write = df.write.saveAsTable()
+}
