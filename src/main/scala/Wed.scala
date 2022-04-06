cb937c6bec2164cf70eefced45bda10052a991d5
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-06 12:44:12 +0200

    commit: new excersise

--- src/main/scala/Wed.scala
+++ src/main/scala/Wed.scala
@@ -1,12 +1,15 @@
 import org.apache.spark.sql.functions._
+
 object Wed extends App {
 
   import org.apache.spark.sql.SparkSession
 
+  val path = if (args(0).nonEmpty) sample100.csv(args(0))
+  else sample100.csv("Sample100.csv")
+
   val spark = SparkSession
     .builder()
     .appName("Spark SQL basic example")
-    //.config("spark.some.config.option", "some-value")
     .master("local[*]")
     .getOrCreate()
 
@@ -14,21 +17,8 @@ object Wed extends App {
     .read
     .option("header", "true")
     .option("inferSchema", "true")
-    .csv("Sample100.csv")
 
-   val newFile =  sample100
-     .withColumn("Employee Markme", upper(sample100("Employee Markme")))
-     .show
+  val newFile = path
+    .withColumn("Employee Markme", upper(path("Employee Markme")))
+    .show
 }
-
-
-
-//  val write = df.write.text("plik4.txt")
-
-// Morning Exercise
-// DataFrame (Dataset)
-// 1. Load CSV file
-// 2. Dataset.withColumn + functions object
-//  - https://spark.apache.org/docs/latest/api/scala/org/apache/spark/sql/functions$.html
-//  - Values should all be UPPER case
-// 3. DataFrame.show
